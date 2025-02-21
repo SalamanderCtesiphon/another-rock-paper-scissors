2340c7408a06b10f87530ccdc417e8dd1e82b14e
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-21 14:40:03 -0600

    added the logic to play a single round of the game

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 ///This is the Rock, Paper, Scissors game
 
 function getComputerChoice() {
-  const choiceArray = ["Rock", "Paper", "Scissors"]
+  const choiceArray = ["Rock", "Paper", "Scissors"];
   return choiceArray.at(Math.floor(Math.random() * 3)); 
-}
+};
 
-let computerChoice = "Scissors"///getComputerChoice()
+///let computerChoice = "Scissors"///getComputerChoice()
 
 function getHumanChoice() {
-  const getUserInput = prompt("Enter either: Rock, Paper or Scissors")
+  const getUserInput = prompt("Enter either: Rock, Paper or Scissors");
   return getUserInput;
-}
+};
 
-let humanChoice = "Rock"///getHumanChoice()
+///let humanChoice = "Rock"///getHumanChoice()
 
-let humanScore = 0
-let computerScore =0
 
-function playRound(computerChoice, humanChoice, humanScore, computerScore) {
+function playRound() {
+  let humanScore = 0;
+  let computerScore = 0;
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+
   if(humanChoice === "Rock" && computerChoice === "Scissors") {
-    humanScore++
-    console.log(humanChoice + " beats " + computerChoice + ". " + "Player's score: " + humanScore) 
-  }
-}
+    humanScore++;
+    console.log(humanChoice + " beats " + computerChoice + ". " + "Player's score: " + humanScore);
+  } else if (humanChoice === "Rock" && computerChoice === "Rock") {
+    console.log(humanChoice + " ties with " + computerChoice + ". ");
+  } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+    computerScore++;
+    console.log(computerChoice + " beats " + humanChoice + ". " + "Computer's score: " + computerScore);
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    humanScore++;
+    console.log(humanChoice + " beats " + computerChoice + ". " + "Player's score: " + humanScore);
+  } else if (humanChoice === "Paper" && computerChoice === "Paper") {
+    console.log(humanChoice + " ties with " + computerChoice + ". ");
+  } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+    computerScore++;
+    console.log(computerChoice + " beats " + humanChoice + ". " + "Computer's score: " + computerScore);
+  } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+    computerScore++;
+    console.log(computerChoice + " beats " + humanChoice + ". " + "Computer's score: " + computerScore);
+  } else if (humanChoice === "Scissores" && computerChoice === "Scissors") {
+    console.log(humanChoice + " ties with " + computerChoice + ". ");
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    humanScore++;
+    console.log(humanChoice + " beats " + computerChoice + ". " + "Player's score " + humanScore);
+  } 
+};
 
-playRound(computerChoice, humanChoice, humanScore, computerScore)
+playRound();
