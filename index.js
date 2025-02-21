aa35f16fa5927633e3b40d45b98b040364d502d4
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-21 03:46:05 -0600

    began coding the logic for the assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hi")
+///This is the Rock, Paper, Scissors game
+
+function getComputerChoice() {
+  const choiceArray = ["Rock", "Paper", "Scissors"]
+  return choiceArray.at(Math.floor(Math.random() * 3)); 
+}
+
+let computerChoice = "Scissors"///getComputerChoice()
+
+function getHumanChoice() {
+  const getUserInput = prompt("Enter either: Rock, Paper or Scissors")
+  return getUserInput;
+}
+
+let humanChoice = "Rock"///getHumanChoice()
+
+let humanScore = 0
+let computerScore =0
+
+function playRound(computerChoice, humanChoice, humanScore, computerScore) {
+  if(humanChoice === "Rock" && computerChoice === "Scissors") {
+    humanScore++
+    console.log(humanChoice + " beats " + computerChoice + ". " + "Player's score: " + humanScore) 
+  }
+}
+
+playRound(computerChoice, humanChoice, humanScore, computerScore)
