b871d25a265e73e9b0337ab9ffe0485a4f7ccc86
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-23 18:17:52 -0600

    moved the global score variables into the play game function

--- index.js
+++ index.js
@@ -13,19 +13,16 @@ function getHumanChoice() {
   return text2;
 };
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanScore, computerScore) { 
+
+function playRound() { 
   let humanChoice = getHumanChoice();
   let computerChoice = getComputerChoice();
 
   if (humanChoice === "Rock" && computerChoice === "Scissors" || 
       humanChoice === "Paper" && computerChoice === "Rock" || 
       humanChoice === "Scissors" && computerChoice === "Paper") {
-      humanScore++;
-      console.log(humanChoice + " beats " + computerChoice + ". " + "Player's score: " + humanScore);
-      return humanScore;
+      console.log(humanChoice + " beats " + computerChoice + ". " + "Player's score: ");
   } else if (humanChoice === "Rock" && computerChoice === "Rock" || 
       humanChoice === "Paper" && computerChoice === "Paper" || 
       humanChoice === "Scissors" && computerChoice === "Scissors") {
@@ -33,18 +30,16 @@ function playRound(humanScore, computerScore) {
   } else if (humanChoice === "Rock" && computerChoice === "Paper" || 
       humanChoice === "Paper" && computerChoice === "Scissors" || 
       humanChoice === "Scissors" && computerChoice === "Rock") {
-      computerScore++;
-      console.log(computerChoice + " beats " + humanChoice + ". " + "Computer's score: " + computerScore);
-      return computerScore;
+      console.log(computerChoice + " beats " + humanChoice + ". " + "Computer's score: ");
   } else { console.log("enter a valid choice: Rock, Paper or Scissors") }
 };
 
 
-function playGame (humanScore, computerScore) {
-  playRound(humanScore, computerScore);
-  humanScore = humanScore;
-  computerScore = computerScore;
+function playGame () {
+  let humanScore = 0;
+  let computerScore = 0;
+  playRound();
   console.log(`PLAYER SCORE: ${humanScore} COMPUTER SCORE: ${computerScore}`);
 };
 
-playGame(humanScore, computerScore);
+playGame();
